d156f0b79d669bdd952ba161695592eb44f248dc
Mohammad Mohabbati <mohabbati@live.com>
2022-08-27 22:19:42 +0300

    Add JWT and AppSettings class

---
+++ src/Gambling.Model/AppSettings.cs
@@ -0,0 +1,22 @@
+﻿namespace Gambling.Model;
+
+public class AppSettings
+{
+    public IdentitySettings IdentitySettings { get; set; }
+
+    public JwtSettings JwtSettings { get; set; }
+}
+
+public class IdentitySettings
+{
+    public int PasswordRequiredLength { get; set; }
+}
+
+public class JwtSettings
+{
+    public string SecretKey { get; set; }
+    public string Issuer { get; set; }
+    public string Audience { get; set; }
+    public int NotBeforeMinutes { get; set; }
+    public int ExpirationMinutes { get; set; }
+}
---
+++ src/Gambling.Service/Dtos/Account/SignInInputDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Gambling.Service.Dtos.Account;
+
+public class SignInInputDto
+{
+    public string? UserName { get; set; }
+
+    public string? Password { get; set; }
+}
