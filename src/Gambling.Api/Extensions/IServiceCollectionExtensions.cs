c97f0647a617a8707e37491bf7b7b45c414d4611
Mohammad Mohabbati <mohabbati@live.com>
2022-08-27 10:33:28 +0300

    Add AspNetCore Identity

---
+++ src/Gambling.Api/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Gambling.Data;
+using Gambling.Model.Account;
+using Microsoft.AspNetCore.Identity;
+
+namespace Gambling.Api.Extensions;
+
+public static class IServiceCollectionExtensions
+{
+    public static void AddGamblingIdentity(this IServiceCollection services)
+    {
+        services.AddIdentity<User, Role>(options =>
+        {
+            options.Password.RequiredLength = 3;
+        }).AddEntityFrameworkStores<GamblingDbContext>().AddDefaultTokenProviders();
+    }
+}
---
+++ src/Gambling.Model/Account/User.cs
@@ -0,0 +1,8 @@
+﻿using Gambling.Model.Common;
+using Microsoft.AspNetCore.Identity;
+
+namespace Gambling.Model.Account;
+
+public class User : IdentityUser<Guid>, IGamblingEntity
+{
+}
