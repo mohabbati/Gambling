f98d223124dfa30f7f8327f3fe305ba693f60165
Mohammad Mohabbati <mohabbati@live.com>
2022-08-28 09:39:42 +0300

    Rafactor app start and settings

---
+++ src/Gambling.Api/Startup/Services.cs
@@ -0,0 +1,29 @@
+﻿using Gambling.Model;
+
+namespace Gambling.Api.Startup;
+
+public static class Services
+{
+    public static void Add(IServiceCollection services, IConfiguration configuration)
+    {
+        var appSettings = configuration.GetSection(nameof(AppSettings)).Get<AppSettings>();
+
+        services.Configure<AppSettings>(configuration.GetSection(nameof(AppSettings)));
+
+        services.AddControllers();
+
+        services.AddEndpointsApiExplorer();
+
+        services.AddGamblingDbContext(configuration);
+
+        services.AddEndpointsApiExplorer();
+
+        services.AddGamblingSwaggerGen();
+
+        services.AddGamblingIdentity(appSettings.IdentitySettings);
+
+        services.AddGamblingJwt(appSettings.JwtSettings);
+
+        services.AddGamblingServices();
+    }
+}
