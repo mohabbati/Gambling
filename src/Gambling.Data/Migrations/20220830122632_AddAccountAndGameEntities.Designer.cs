a1c480f843d0829bb2993d21780b75276988f549
Mohammad Mohabbati <mohabbati@live.com>
2022-08-30 15:30:36 +0300

    Add Account and Game model

--- src/Gambling.Data/Migrations/20220830122632_AddAccountAndGameEntities.Designer.cs
+++ src/Gambling.Data/Migrations/20220830122632_AddAccountAndGameEntities.Designer.cs
@@ -4,6 +4,7 @@ using Gambling.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Gambling.Data.Migrations
 {
     [DbContext(typeof(GamblingDbContext))]
-    partial class GamblingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220830122632_AddAccountAndGameEntities")]
+    partial class AddAccountAndGameEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
