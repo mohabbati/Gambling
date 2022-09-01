8a48cca8bec1871724069843a50a5ae9cba5fc19
mohabbati <mohabbati@live.com>
2022-09-01 22:42:57 +0300

    Add sequential guid for IGamblingEntity Ids (#22)
    * Add sequential guid for Ids
    
    * Add migration

---
+++ src/Gambling.Data/Migrations/20220901194046_AddSequentialGuidForIds.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Gambling.Data.Migrations
+{
+    public partial class AddSequentialGuidForIds : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "Plays",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValueSql: "NewSequentialID()",
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "AspNetUsers",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValueSql: "NewSequentialID()",
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "AspNetRoles",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValueSql: "NewSequentialID()",
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "AccountTransactions",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValueSql: "NewSequentialID()",
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "Accounts",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValueSql: "NewSequentialID()",
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "Plays",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldDefaultValueSql: "NewSequentialID()");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "AspNetUsers",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldDefaultValueSql: "NewSequentialID()");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "AspNetRoles",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldDefaultValueSql: "NewSequentialID()");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "AccountTransactions",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldDefaultValueSql: "NewSequentialID()");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "Accounts",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldDefaultValueSql: "NewSequentialID()");
+        }
+    }
+}
