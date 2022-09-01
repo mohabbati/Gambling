73209e5cc3eacaaf9a4168737c7bf213c6b34e65
Mohammad Mohabbati <mohabbati@live.com>
2022-09-01 22:39:56 +0300

    Add sequential guid for Ids

--- src/Gambling.Data/GamblingDbContext.cs
+++ src/Gambling.Data/GamblingDbContext.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Gambling.Models.Identity;
 using Gambling.Models.Game;
 using Gambling.Models.Account;
+using Gambling.Models.Common;
 
 namespace Gambling.Data;
 
@@ -16,4 +18,26 @@ public class GamblingDbContext : IdentityDbContext<User, Role, Guid, UserClaim, 
     public virtual DbSet<Account> Accounts { get; set; } = default!;
     public virtual DbSet<AccountTransaction> AccountTransactions { get; set; } = default!;
     public virtual DbSet<Play> Plays { get; set; } = default!;
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        AddSequentialGuidForIds(builder);
+    }
+
+    private void AddSequentialGuidForIds(ModelBuilder builder)
+    {
+        foreach (IMutableEntityType entityType in builder.Model.GetEntityTypes())
+        {
+            IMutableProperty? property = entityType.GetProperties().SingleOrDefault(p => p.Name.Equals(nameof(IGamblingEntity.Id), StringComparison.OrdinalIgnoreCase));
+            if (property is not null && property.ClrType == typeof(Guid))
+            {
+                var p = builder.Entity(entityType.ClrType).Property(property.Name);
+
+                p.ValueGeneratedOnAdd()
+                    .HasDefaultValueSql("NewSequentialID()");
+            }
+        }
+    }
 }
